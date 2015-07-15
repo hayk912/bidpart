cfc351ea7fe12a2760b47fde55b0665aa1ee0406
hayk912 <haykhovh07@gmail.com>
2015-07-15 12:15:11 +0400

    Update bidpartherokuappcom.py

--- application/local_settings/bidpartherokuappcom.py
+++ application/local_settings/bidpartherokuappcom.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': 'bidpart_django',                            # Or path to database file if using sqlite3.
         'USER': 'litonkhan',                                 # Not used with sqlite3.
         'PASSWORD': '55femtiofem654',                           # Not used with sqlite3.
-        'HOST': '75.126.24.94',                              # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'web389.webfaction.com',                              # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                                          # Set to empty string for default. Not used with sqlite3.
     }
 }
