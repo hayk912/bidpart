1b60ddd5aa1f69dfef5a37f6c65ef8743ffcb14e
hayk <haykhovh07@gmail.com>
2015-07-15 15:13:43 +0400

    db

--- application/local_settings/bidpartherokuappcom.py
+++ application/local_settings/bidpartherokuappcom.py
@@ -5,9 +5,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',                # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bidpart_django',                            # Or path to database file if using sqlite3.
         'USER': 'litonkhan',                                 # Not used with sqlite3.
-        'PASSWORD': '55femtiofem654',                           # Not used with sqlite3.
-        'HOST': 'web389.webfaction.com',                              # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                                          # Set to empty string for default. Not used with sqlite3.
+        'PASSWORD': '55femtiofem',                           # Not used with sqlite3.
+        'HOST': '75.126.24.94',                              # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                                          # Set to empty string for default. Not used with sqlite3.
     }
 }
 
