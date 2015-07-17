177b85040ab3264c26aa0547692342c08e23ee51
hayk <haykhovh07@gmail.com>
2015-07-17 12:44:14 +0400

    db changes

--- application/local_settings/bidpartherokuappcom.py
+++ application/local_settings/bidpartherokuappcom.py
@@ -3,9 +3,9 @@ ENVIRONMENT = "production"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',                # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bidpart_django',                            # Or path to database file if using sqlite3.
-        'USER': 'litonkhan',                                 # Not used with sqlite3.
-        'PASSWORD': '55femtiofem',                           # Not used with sqlite3.
+        'NAME': 'python',                            # Or path to database file if using sqlite3.
+        'USER': 'adminmLI8wna',                                 # Not used with sqlite3.
+        'PASSWORD': 'hNQHrq6UzscH',                           # Not used with sqlite3.
         'HOST': '75.126.24.94',                              # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                                          # Set to empty string for default. Not used with sqlite3.
     }
